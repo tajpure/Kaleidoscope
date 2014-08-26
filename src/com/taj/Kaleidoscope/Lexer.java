15711f7312c184e58c6976d4a101af1b440e3d5b
tajpure <tajpure@gmail.com>
2014-08-26 19:29:59 +0800

    Update Lexer

--- src/com/taj/Kaleidoscope/Lexer.java
+++ src/com/taj/Kaleidoscope/Lexer.java
@@ -9,50 +9,49 @@ import java.util.Scanner;
  */
 public class Lexer {
 	
-	private static String IdentifierStr = "";
+	private static String IdentifierStr;
 	
 	private static double NumVal = 0;
 	
 	public static int gettok(char[] input) {
 		int indexOfInput = 0;
-		char LastChar = input[indexOfInput];
-		while (Character.isSpaceChar(LastChar)) {
-			LastChar = input[indexOfInput++];
-		}
-			if (Character.isAlphabetic(LastChar)) {
+		IdentifierStr = "";
+		char LastChar = input[indexOfInput++];
+		
+		if (Character.isAlphabetic(LastChar)) {
+			IdentifierStr += LastChar;
+			while (indexOfInput < input.length && Character.isAlphabetic(LastChar = input[indexOfInput++])) {
 				IdentifierStr += LastChar;
-				while (Character.isDigit(LastChar = input[indexOfInput++])) {
-					IdentifierStr += LastChar;
-					if ("def".equals(IdentifierStr)) {
-						return Token.TOK_DEF;
-					} else if ("extern".equals(IdentifierStr)) {
-						return Token.TOK_EXTERN;
-					}
+				if ("def".equals(IdentifierStr)) {
+					return Token.TOK_DEF;
+				} else if ("extern".equals(IdentifierStr)) {
+					return Token.TOK_EXTERN;
 				}
-			} else if (Character.isDigit(LastChar) || ".".equals(LastChar)) {
-				String NumStr = "";
-				do {
-					NumStr += LastChar;
-					LastChar = input[indexOfInput++];
+			}
+		} else if (Character.isDigit(LastChar) || ".".equals(LastChar)) {
+			String NumStr = "";
+			do {
+				NumStr += LastChar;
+				LastChar = input[indexOfInput++];
 				} while (Character.isDigit(LastChar) || ".".equals(LastChar));
 				
 				NumVal = Double.parseDouble(NumStr);
 				return Token.TOK_NUMBER;
-			} else if (LastChar == '#') {
-				do {
-					LastChar = input[indexOfInput++];
-				} while (indexOfInput != input.length && LastChar != '\n' && LastChar != '\r');
-				
-				if (indexOfInput != input.length) {
-					return gettok(input);
-				}
+		} else if (LastChar == '#') {
+			do {
+				LastChar = input[indexOfInput++];
+			} while (indexOfInput != input.length && LastChar != '\n' && LastChar != '\r');
+			
+			if (indexOfInput != input.length) {
+				return gettok(input);
 			}
-			if (indexOfInput == input.length) {
-				return Token.TOK_EOF;
-			}
-			int ThisChar = LastChar;
-			LastChar = input[indexOfInput];
-			return ThisChar;
+		}
+		if (indexOfInput == input.length) {
+			return Token.TOK_EOF;
+		}
+		int ThisChar = LastChar;
+		LastChar = input[indexOfInput];
+		return ThisChar;
 	}
 	
 	public static void main(String[] args) {
@@ -64,6 +63,7 @@ public class Lexer {
 	    	 if (line == null || line.length() == 0) {
 	    		 continue;
 	    	 }
+	    	 line = line.trim();
 	    	 System.out.println(gettok(line.toCharArray()));
 	     }
 	}
